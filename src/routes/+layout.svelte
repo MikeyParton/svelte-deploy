e96c0877f8380a6c6dee4b8986fe7cc28820b6fc
mikeyparton <mikerelated@gmail.com>
2023-07-10 08:37:43 +1000

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { version } from '$app/environment';
+  export const prerender = true;
+</script>
+
+{version}
+
+<slot />
---
+++ svelte.config.js
@@ -0,0 +1,27 @@
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import * as child_process from "node:child_process";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
+    version: {
+      name: child_process.execSync("git rev-parse HEAD").toString().trim(),
+    },
+  },
+};
+
+export default config;
